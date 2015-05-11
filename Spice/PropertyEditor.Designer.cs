d88cbdf8438442ab34c5f6b8b7eefafe1008e965
andyubird <andyubird@gmail.com>
2015-05-11 12:17:01 +0800

    .

--- Spice/PropertyEditor.Designer.cs
+++ Spice/PropertyEditor.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(172, 60);
             this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "PropertyEditor";
